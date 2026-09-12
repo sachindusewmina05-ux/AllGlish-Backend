429b4f7f38d4ddb51bedc0b92b5a4c20344dec40
sachindusewmina05-ux <sachindusewmina05@gmail.com>
2026-09-12 23:53:48 +0530

    Update and rename main.py to index.py

--- index.py
+++ index.py
@@ -263,4 +263,6 @@ async def filter_srt(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000
+    from mangum import Mangum
+handler = Mangum(app)
