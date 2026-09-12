28505b4a0c11751a54250119cb5917a05e5e14cf
sachindusewmina05-ux <sachindusewmina05@gmail.com>
2026-09-12 22:08:11 +0530

    main.py

--- main.py
+++ main.py
@@ -263,3 +263,5 @@ async def filter_srt(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    from mangum import Mangum
+handler = Mangum(app)
